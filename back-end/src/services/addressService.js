e2c9cbf228030f858351cbcf187f180529c8f1dd
lemoscaio <caio_lemos94@hotmail.com>
2022-10-28 21:09:44 -0300

    fix: change axios to fetch

--- back-end/src/services/addressService.js
+++ back-end/src/services/addressService.js
@@ -5,8 +5,11 @@ const CEP_API_URL = "https://ws.apicep.com/cep.json?code="
 export const addressService = {
   async getAddressByCEP(cep) {
 
-    const {data} = await axios.get(`${CEP_API_URL}${cep}`)
+    // const {data} = await axios.get(`${CEP_API_URL}${cep}`)
+    const result = await fetch(`${CEP_API_URL}${cep}`).then(res => res.json())
+    
+    console.log(result)
    
-    return data
+    return result
   }
 }
