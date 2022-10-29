fd0c017f41e69300f58d62815b12bb221235be4d
lemoscaio <caio_lemos94@hotmail.com>
2022-10-29 08:29:50 -0300

    fix: remove axios imports and dependencies

--- back-end/src/services/addressService.js
+++ back-end/src/services/addressService.js
@@ -1,11 +1,8 @@
-import axios from "axios"
-
 const CEP_API_URL = "https://ws.apicep.com/cep.json?code="
 
 export const addressService = {
   async getAddressByCEP(cep) {
 
-    // const {data} = await axios.get(`${CEP_API_URL}${cep}`)
     const result = await fetch(`${CEP_API_URL}${cep}`).then(res => res.json())
     
     console.log(result)
